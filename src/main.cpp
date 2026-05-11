0b965a93049dd6363fadf3459d1afa8c37f15f66
Arda <minecarman@gmail.com>
2026-05-12 02:09:12 +0300

    understandable variables

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,49 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_BME280.h>
+#include <MPU6050.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-Adafruit_BME280 bme; 
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+Adafruit_BME280 bme;
+MPU6050 mpu;
 
 void setup() {
+  Serial.begin(9600);
   lcd.init();
   lcd.backlight();
   
   if (!bme.begin(0x76)) {
-    lcd.print("BME Bulunamadi!");
+    lcd.print("BME Hatasi!");
     while (1);
   }
+  
+  mpu.initialize();
+  if (!mpu.testConnection()) {
+    lcd.print("MPU Hatasi!");
+    while (1);
+  }
+
+  lcd.clear();
+  lcd.print("CycloPath Hazir!");
+  delay(2000);
 }
 
 void loop() {
   float temp = bme.readTemperature();
-  float alt = bme.readAltitude(1013.25);
+  float alt = bme.readAltitude(1013.25); 
 
+  int16_t ax, ay, az;
+  mpu.getAcceleration(&ax, &ay, &az);
+  float angle = atan2(ax, az) * 180 / PI;
+
+  lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Sicaklik: ");
-  lcd.print(temp);
-  lcd.print(" C");
+  lcd.print("Alt: "); lcd.print(alt, 0); lcd.print("m ");
+  lcd.print("T: "); lcd.print(temp, 0); lcd.print("C");
 
   lcd.setCursor(0, 1);
-  lcd.print("Rakim: ");
-  lcd.print(alt);
-  lcd.print(" m");
+  lcd.print("Egim: %"); 
+  lcd.print(tan(angle * PI / 180) * 100, 1);
 
-  delay(2000);
+  delay(1000);
 }
